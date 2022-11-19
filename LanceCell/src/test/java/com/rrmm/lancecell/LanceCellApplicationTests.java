5dc039ce42e2e9b1441fa677c03be4ce0585354e
Riyad Busttami <Riyad.Bustami@gmail.com>
2022-11-19 13:42:29 +0200

    Initial commit

---
+++ LanceCell/src/test/java/com/rrmm/lancecell/LanceCellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrmm.lancecell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanceCellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
